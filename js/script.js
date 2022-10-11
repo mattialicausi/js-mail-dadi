8ee26cfa3f874039cb32955404f5ca83cdc57c67
mattialicausi <licausivitomattia@gmail.com>
2022-10-11 17:06:12 +0200

    aggiunto secondo esercizio

--- js/script.js
+++ js/script.js
@@ -28,16 +28,63 @@
                 
                 if(valore == email){
                     console.log('Sei stato invitato');
-                    risposta.innerHTML = 'Sei stato invitato';
+                    risposta.innerHTML = ('Sei stato invitato');
             
                 } else{
                     console.log('Non sei stato invitato');
-                    risposta.innerHTML = 'Non sei stato invitato';
+                    risposta.innerHTML = ('Non sei stato invitato');
                 }
-                
             }
           }
+
+
+
+
+        //   Gioco dei dadi
+        //   Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+        //   Stabilire il vincitore, in base a chi fa il punteggio più alto.
+        //   Prima di partire a scrivere codice poniamoci qualche domanda:
+        //   Che ci sia un array da qualche parte?
+        //   Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+        //   Consigli del giorno:
+        //   1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+        //   2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+        //   3. si ma noi cosa vogliamo fare?
+        //   4. torniamo a scrivere in italiano
+        //   5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
         
 
 
+        function lanciaDadi(){
+
+            const numeroUtente = [];
+            const numeroComputer = [];
+
+            const dadiUtente = document.getElementById('dadiUtente');
+            const dadiComputer = document.getElementById('dadiComputer');
+
+            const numeroDaGenerare = 1;
+
+                let numeroGeneratoUtente = Math.floor(Math.random() * 7);
+                console.log(numeroGeneratoUtente);
+
+                let numeroGeneratoComputer = Math.floor(Math.random() * 7);
+                console.log(numeroGeneratoComputer);
+
+
+
+
+                if (numeroGeneratoUtente > numeroGeneratoComputer){
+                    console.log('Ha vinto l utente');
+                    dadiComputer.append = numeroGeneratoUtente;
+                    dadiComputer.append = numeroGeneratoComputer;
+                } else if (numeroGeneratoUtente == numeroGeneratoComputer){
+                    console.log('Pareggio');
+                } else{
+                    console.log('Ha vinto il computer');
+                }
+           
+
+
+        }
 
